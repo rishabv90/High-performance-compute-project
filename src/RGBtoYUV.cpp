fc0cddd9b6ac71b177da5bfcd3d0949712d6a49a
Fabian Montero <fdlpm@email.arizona.edu>
2021-04-24 20:23:28 -0700

    RGBtoYUV and Smooth files to src

--- src/RGBtoYUV.cpp
+++ src/RGBtoYUV.cpp
@@ -5,7 +5,102 @@
 
 #define CHANNELS 3 // we have 3 channels corresponding to RGB
 __global__ void YUVandCItoGray(float * grayImage, float * yuvImage, float * rgbImage, float * redData, float * greenData, float * blueData, int width, int height) {//this function combineds yuv, grayscale, and color invarient image processing and also assigns R G B arrays for coalesced reads in proc 5 and reduces register trafic
+
+/*
+
+//simple copy ??
+int x = threadIdx.x + blockIdx.x * blockDim.x; //col
+int y = threadIdx.y + blockIdx.y * blockDim.y; //row
+
+if(x < width && y < height){
+	int rgbOffset = (y * width + x);
 	
+	float r = rgbImage[rgbOffset];
+	float g = rgbImage[rgbOffset + 1];
+	float b = rgbImage[rgbOffset + 2];
+
+	yuvImage[rgbOffset] = r;
+	yuvImage[rgbOffset + 1] = g;
+	yuvImage[rgbOffset + 2] = b;
+	
+
+}
+*/
+
+/*
+//printf("width = %d and height = %d\n\n ",width,height);
+int x = threadIdx.x + blockIdx.x * blockDim.x;
+int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+if(x < width && y < height){ //thread divergence here
+	//int grayOffset = y * width + x;
+ 	int rgbOffset = (y * width + x)*3 ;
+	
+	float r = rgbImage[rgbOffset];
+	float g = rgbImage[rgbOffset + 1];
+	float b = rgbImage[rgbOffset + 2];
+
+	float ci1 = 0;
+	float ci2 = 0;
+	float ci3 = 0; 	
+
+
+
+	if(atan(r * max(g,b)) < 0){
+		ci1 = 0; 	
+	}else {
+		ci1 = atan(r * max(g,b));
+	}
+
+
+	if(atan(g * max(r,b)) < 0){
+		ci2 = 0; 	
+	}else {
+		ci2 = atan(g * max(r,b));
+	}
+
+
+	if(atan(b * max(r,g)) < 0){
+		ci3 = 0; 	
+	}else {
+		ci3 = atan(b * max(r,g));
+	}
+
+
+	yuvImage[rgbOffset] = ci1;
+	yuvImage[rgbOffset + 1] = ci2;
+	yuvImage[rgbOffset + 2] = ci3;  */
+
+
+
+//greyscale
+
+int x = threadIdx.x + blockIdx.x * blockDim.x;
+int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+if(x < width && y < height){ //thread divergence here // greyscale 
+	int grayOffset = y * width + x;
+ 	int rgbOffset = (y * width + x)*3 ;
+	
+	float r = rgbImage[rgbOffset];
+	float g = rgbImage[rgbOffset + 1];
+	float b = rgbImage[rgbOffset + 2];
+
+	//grayImage[grayOffset] = 0.21f*r + 0.71f*g + 0.07f*b; //greyscale output image
+
+	
+	float ci1 = atan(r * max(g,b));
+	float ci2 = atan(g * max(r,b));
+	float ci3 = atan(b * max(r,g));
+
+	yuvImage[grayOffset] = ci1;
+	yuvImage[grayOffset + 1] = ci2;
+	yuvImage[grayOffset + 2] = ci3;
+
+	
+	
+}
+
 }
 
 
--- src/Smooth.cpp
+++ src/Smooth.cpp
@@ -2,7 +2,8 @@
 #define SMOOTH_CPP
 
 //@@ DEFINE TILE SIZE AND INSERT DEVICE CODE HERE
-#define TILE_HEIGHT 16
+#define IN_TILE_HEIGHT 16
+#define OUT_TILE_HEIGHT 8
 #define MASK_WIDTH 5
 #define MASK_RADIUS MASK_WIDTH/2
 #define AREA ( MASK_WIDTH + TILE_HEIGHT -1 )
@@ -20,10 +21,38 @@
 //		imgWidth = ImageIn's & ImageOut's width
 //		imgHeight = ImageIn's & ImageOut's height
 __global__ void smooth_kernel(float *In, float *Out, const float* __restrict__ K, int channels, int imgWidth, int imgHeight) {
-	// Using shared memory
-	// Want more data than the mask area.
-	// Need Area x Area of Shared Memory ( Area = mask + tile -1 )
-	
+
+    // The size of each thread block matches the size of an input tile
+    __shared__ float ds_In[IN_TILE_HEIGHT][IN_TILE_HEIGHT];
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int Row_o = blockIdx.y * OUT_TILE_HEIGHT  + ty;
+    int Col_o = blockIdx.x * OUT_TILE_HEIGHT  + tx;
+
+    int Row_i = Row_o - MASK_RADIUS;
+    int Col_i = Col_o - MASK_RADIUS;
+
+
+    if(Row_i >= 0 && Row_i < imgHeight && Col_i >= 0 && Col_i < imgWidth)
+        ds_In[ty][tx] = In[Row_i*imgWidth + Col_i];
+    else
+        ds_In[ty][tx] = 0;
+
+
+    __syncthreads();
+
+
+    if(tx < OUT_TILE_HEIGHT && ty < OUT_TILE_HEIGHT){
+        float pixVal = 0;
+        for(int y=0; y < MASK_WIDTH; y++)
+            for(int x=0; x < MASK_WIDTH; x++)
+                pixVal += K[y*MASK_WIDTH + x] * ds_In[y+ty][x+tx];
+
+        if(Row_o < imgHeight && Col_o < imgWidth)
+            Out[Row_o*imgWidth + Col_o] = (unsigned char)(pixVal);
+    } 
 }
 
 //-----------------------------------------------------------------------//
@@ -43,7 +72,9 @@ __global__ void smooth_kernel_row(float *In, float *Out, const float* __restrict
     // 1D Row Shared Memory must be of size tile x area.
     // We still use area because we still need the padding in all the columns, while the 
     // rows are confined to the tile itself.
-	
+
+
+
 }
 //-----------------------------------------------------------------------//
 //
@@ -75,6 +106,23 @@ __global__ void smooth_kernel_col(float *In, float *Out, const float* __restrict
 //		imgHeight = ImageIn's & ImageOut's height
 __global__ void smooth_kernel_global(float *In, float *Out, const float* __restrict__ K, int channels, int imgWidth, int imgHeight) {
 	// gLoc = our global Location.
-	
+	int Col = (blockIdx.x * blockDim.x + threadIdx.x);
+    int Row = (blockIdx.y * blockDim.y + threadIdx.y);
+    if(Col < imgWidth && Row < imgHeight) {
+        float pixVal = 0;
+        int start_col = Col - (MASK_WIDTH/2);
+        int start_row = Row - (MASK_WIDTH/2);
+        
+        for (int y=0;y<MASK_WIDTH;++y) {
+            for (int x=0;x<MASK_WIDTH;++x) {
+                int curRow = start_row + y;
+                int curCol = start_col + x;
+                if (curRow > -1 && curRow < imgHeight && curCol > -1 && curCol < imgWidth){ // >= 0
+                    pixVal += In[curRow*imgWidth + curCol] * K[y*MASK_WIDTH + x];
+                }                    
+            }
+        }
+        Out[Row*imgWidth+Col] = (unsigned char)(pixVal);
+    }
 }
 #endif // SMOOTH_CPP
